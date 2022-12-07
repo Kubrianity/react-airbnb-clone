5bbfedd326b6c0b4e63d0c7eec6eb683d3b2b5b6
Kubrianity <kubraaozerenn@gmail.com>
2022-12-07 22:40:39 +0300

    Create and style main section component

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+  return (
+    <section>
+      <img src =  "./images/section-photo.png" className = "section-photo" alt = "random-images" />
+      <h1 className="section-header">Unforgettable activities hosted by locals</h1>
+      <p className = "section-text">Join unique interactive activities led by
+      one-of-a-kind hosts—all without leaving home.</p>
+    </section>
+  )
+}
